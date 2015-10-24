50b2c872a0cf5af174f1caa2122294fbfa9839d5
menka.ix <mendrika.irina@gmail.com>
2015-10-25 01:23:51 +0300

    bomb OK

---
+++ gameJam2015/Assets/Scripts/Bomb.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+
+    public float radius ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Ennemy ennemy = other.gameObject.GetComponent<Ennemy>();
+
+        if(ennemy != null)
+        {
+            GameObject.Destroy(ennemy.gameObject);
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, radius, transform.forward,0);
+
+            foreach(RaycastHit hit in hits)
+            {
+                Ennemy enn = hit.transform.gameObject.GetComponent<Ennemy>();
+                if(enn != null)
+                {
+                    GameObject.Destroy(enn.gameObject);
+                }
+
+            }
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
