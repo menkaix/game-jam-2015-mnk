96e73d9acfff5f514a8ade81aaae2b2b80f8d94a
menkaix <mendrika.irina@gmail.com>
2015-10-24 11:18:57 +0300

    first

---
+++ gameJam2015/Assets/FollowMouse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)) {
+			//gameObject.transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+			//Vector3 targetLookAt = hit.point - transform.position ;
+
+			transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+		}
+
+	}
+}
---
+++ gameJam2015/Assets/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 0.25f ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x + speed * Input.GetAxis("Horizontal"), transform.position.y, transform.position.z + speed * Input.GetAxis("Vertical"));
+	}
+}
