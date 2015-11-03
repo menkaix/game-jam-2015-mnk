3337c90cc8cffbb10afc97da01752440b43fabfd
adriela <adriela_ratefiarivony@hotmail.fr>
2015-11-03 11:00:36 +0300

    last
    last

---
+++ gameJam2015/Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+	public int indice;
+	public Sprite sp;
+	public string nb;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
